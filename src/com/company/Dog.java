7b4a8f27b26096598e16d8ec385be0d7ed94ad79
AsylChan <aseka.qanatbek@bk.ru>
2021-11-17 23:08:58 +0600

    initial commit

---
+++ src/com/company/Dog.java
@@ -0,0 +1,24 @@
+package com.company;
+public class Dog extends Animals {
+    public Dog(String name) {
+        super(name);
+    }
+
+    public void makeJump() {
+        System.out.println("Ya prygayu");
+    }
+
+    public void makeVoice() {
+        System.out.println("Gav-gav!");
+    }
+
+    public void makeRun() {
+        System.out.println("Begat...");
+    }
+
+    public void makeBite() {
+        System.out.println("Kusat...");
+    }
+}
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        Dog puppy=new Dog("Sebiok");
+        puppy.showName();
+        puppy.makeVoice();
+        puppy.makeBite();
+        puppy.makeJump();
+        puppy.makeRun();
+
+    }
+}
